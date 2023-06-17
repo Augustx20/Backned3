18de5a805ad6508276ea574bdff1eaf8d8fb0ad8
Augusto Machado <augus1726@gmail.com>
2023-06-17 19:37:59 -0300

    subida de population with students and users

--- src/DAO/models/users.model.js
+++ src/DAO/models/users.model.js
@@ -4,8 +4,8 @@ import { Schema, model } from "mongoose";
 export const UserModel = model(
     "users", //nombre de collection donde se va  hacer el CRUD
     new Schema({
-        firstName: {type: String, required: true},
-        lastName: {type: String, required: true},
+        firstName: {type: String, required: true,index:true},
+        lastName: {type: String, required: true,index:true},
         email: {type: String, required: true},
 
     })
--- src/utils/connections.js
+++ src/utils/connections.js
@@ -1,4 +1,9 @@
+
 import { connect } from "mongoose";
+import { UserModel } from "../DAO/models/users.model.js";
+import { StudentsModel } from "../DAO/models/students.model.js";
+import { CoursesModel } from "../DAO/models/courses.model.js";
+//import { faker } from '@faker-js/faker';
 
 export async function connectMongo(){
   try {
@@ -6,9 +11,55 @@ export async function connectMongo(){
       "mongodb+srv://augus1726:diXbIUoo4Ut9mo73@codercluster.oeptjle.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("plug to mongo!");
+
+    let student = await StudentsModel.find().populate('courses.course');
+    console.log(JSON.stringify(student, null, 2))
+
+
+    // let students = await StudentsModel.findOne({_id:'648e27bd464dc72142471291'});
+    // students.courses.push({course: '648e2941190bb22e37dd3349'});
+    // let res = await StudentsModel.updateOne({_id: '648e27bd464dc72142471291'}, students)
+
+    // const created = await CoursesModel.create({
+    //   title: 'fronted',
+    //   description: 'wonderfull backend courses',
+    //   dificulty: 10,
+    //   professor:'guille'
+    // })
+
+    //const created = StudentsModel.create({
+    //   first_name: 'Julian',
+    //   last_name: 'Barberis',
+    //   email: 'juli@gmail.com',
+    //   gender: 'masculino',
+    //   courses:[]
+    // })
+
+    // let res = await UserModel.find({firstName: 'John'}).explain('executionStats');
+    // console.log(res)
+
+    // (async () => {
+    //   const users = [];
+    //   for (let i = 0; i < 7000; i++) {
+    //     users.push({
+    //       firstName:faker.internet.userName(),
+    //       lastName: faker.internet.userName(),
+    //       email: faker.internet.email()
+    //     })
+    //     try {
+    //       await UserModel.insertMany(users);
+    //       console.log('inserted', users.length, 'users')
+          
+    //     } catch (e) {
+    //       console.error("Error en insert many: ",e)
+          
+    //     }
+    //   }
+    //})();
     
   } catch (e) {
     console.log(e);
     throw "can not connect to the db";
   }
+  
 }
